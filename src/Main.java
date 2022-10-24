e2a1a64e7dcdb65361f8afb8a7f8f04136c70bd5
me <me@java.pl>
2022-10-24 11:19:57 +0200

    Lab 1 - Zadanie 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
         //Lab 1 - Zadanie 1
         Osoba();
+
+        //Lab 1 - Zadanie 2
+        System.out.println("Wprowadz pierwsza liczbe: ");
+        double a = getDouble();
+        System.out.println("Wprowadz druga liczbe: ");
+        double b = getDouble();
+        wypisz(a, b);
     }
 
     //Lab 1 - Zadanie 1
@@ -18,6 +25,21 @@ public class Main {
 
     public static int getWiek() {
         return 23;
+    }
+
+    //Lab 1 - Zadanie 2
+    public static void wypisz(double a, double b) {
+        System.out.println("Suma: " + (a + b));
+        System.out.println("Roznica: " + (a - b));
+        System.out.println("Iloczyn: " + (a * b));
+    }
+
+    //Scannery
+    public static int getInt() {
+        return new Scanner(System.in).nextInt();
+    }
+    public static double getDouble() {
+        return new Scanner(System.in).nextDouble();
     }
 
 }
