717a27243cad9bd0c706907d419f0ca111ecc1ef
me <me@java.pl>
2022-10-24 10:31:58 +0200

    Lab 1 - zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Osoba();
     }
+
+    //Lab 1 - Zadanie 1
+    public static void Osoba() {
+        System.out.println("Imię " + getImie() + ", wiek " + getWiek());
+    }
+
+    public static String getImie() {
+        return "Darek";
+    }
+
+    public static int getWiek() {
+        return 23;
+    }
+
 }
